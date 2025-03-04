311e389d48ba7c24b419cf25a258240a19796052
hugodiazroa <hugodiazroa@gmail.com>
2025-03-04 15:47:28 -0300

    tls 1.3 mostly done (only small numbers work for diffie hellman)

--- all_in_a_script.py
+++ all_in_a_script.py
@@ -3,15 +3,15 @@ from tkinter import scrolledtext
 import  random
 import time
 import gnupg
+import hashlib
 
 
 # Initialize GPG objects
 gpgClient = gnupg.GPG()
 gpgServer = gnupg.GPG()
 
-# Generate new key pairs todo: fix this
-input_dataClient = gpgClient.gen_key_input(name_email='client@example.com', passphrase='client')
-input_dataServer = gpgServer.gen_key_input(name_email='server@example.com', passphrase='server')
+input_dataClient = gpgClient.gen_key_input()
+input_dataServer = gpgServer.gen_key_input()
 keyClient = gpgClient.gen_key(input_dataClient)
 keyServer = gpgServer.gen_key(input_dataServer)
 
@@ -28,14 +28,14 @@ private_keyServer = gpgServer.export_keys(keyServer.fingerprint, True, passphras
 
 
 class TLSVisualizerApp:
-    def __init__(self, raiz):
+    def __init__(self, rain):
         self.debug = False
         self.log_text = None
         self.reset_button = None
         self.stop_button = None
         self.start_button = None
         self.debug_button = None
-        self.root = raiz
+        self.root = rain
         self.root.title("TLS Visualizer")
         self.root.geometry("800x600")  # Set the initial size of the window
         self.root.configure(bg="black")  # Set the background color to black
@@ -71,16 +71,17 @@ class TLSVisualizerApp:
 
         self.log_text.insert(tk.END, "Simulation of TLS 1.2\n\n", "explanation")
         self.log_text.insert(tk.END, "Server Parameters\n", "explanation")
-        #todo: fix this grammar
         self.log_text.insert(tk.END, "TLS uses a X.509 certificate, but for sake of simplicity we will use PGP\n","note")
-
         self.log_text.insert(tk.END, "Certificate:\n", "server")
-
+        self.log_text.insert(tk.END, "PGP\n", "server")
         self.log_text.insert(tk.END, "Public Key:\n", "server")
-
-        self.log_text.insert(tk.END, "Private Key:\n", "server")
-
-        self.log_text.insert(tk.END, "\nHandshake\n\n", "explanation")
+        self.log_text.insert(tk.END, public_keyServer, "server")
+        #time.sleep(1)  # Add a one-second delay here
+        self.log_text.insert(tk.END, "\nPrivate Key:\n", "server")
+        self.log_text.insert(tk.END, private_keyServer, "server")
+        #time.sleep(1)  # Add a one-second delay here
+        self.log_text.see(tk.END)
+        self.log_text.insert(tk.END, "\n\nHandshake\n\n", "explanation")
         self.log_text.insert(tk.END, "ClientHello\n\n", "explanation")
 
         self.log_text.insert(tk.END, "The client sends a ClientHello message to the server, containing the following information:\n", "explanation")
@@ -138,12 +139,12 @@ class TLSVisualizerApp:
         self.log_text.insert(tk.END, str(bin(session_id)) + "\n", "server")
 
         self.log_text.insert(tk.END, "Chosen ciphersuite:\n", "server")
-        self.log_text.insert(tk.END, "0x1303\n", "server")
+        self.log_text.insert(tk.END, "0x1301\n", "server")
 
         self.log_text.insert(tk.END, "Extensions:\n", "server")
         self.log_text.insert(tk.END, "extensions = []\n", "server")
 
-        time.sleep(1)  # Add a one-second delay here
+        #time.sleep(1)  # Add a one-second delay here
 
         self.log_text.insert(tk.END, "\n\nServer send certificate chain\n\n", "explanation")
         self.log_text.insert(tk.END, "The server sends a certificate chain to the client")
@@ -169,24 +170,89 @@ class TLSVisualizerApp:
 
         self.log_text.insert(tk.END, "Simulation of TLS 1.3\n\n", "explanation")
         self.log_text.insert(tk.END, "Server Parameters\n", "explanation")
-
+        self.log_text.insert(tk.END, "TLS uses a X.509 certificate, but for sake of simplicity we will use PGP\n","note")
         self.log_text.insert(tk.END, "Certificate:\n", "server")
-
+        self.log_text.insert(tk.END, "PGP\n", "server")
         self.log_text.insert(tk.END, "Public Key:\n", "server")
-
-        self.log_text.insert(tk.END, "Private Key:\n", "server")
+        self.log_text.insert(tk.END, public_keyServer, "server")
+        self.log_text.insert(tk.END, "\nPrivate Key:\n", "server")
+        self.log_text.insert(tk.END, private_keyServer, "server")
 
         self.log_text.insert(tk.END, "\nHandshake\n\n", "explanation")
         self.log_text.insert(tk.END, "ClientHello\n\n", "explanation")
 
         self.log_text.insert(tk.END, "The client sends a ClientHello message to the server, containing the following information:\n", "explanation")
-
+        self.log_text.insert(tk.END, "Highest version supported:\n", "client")
+        self.log_text.insert(tk.END, "1.3\n", "client")
+        prime = 10007
+        g = 2
+        c = random.getrandbits(10)
+        gc = pow(g, c)
+        gcmodp= gc % prime
+        self.log_text.insert(tk.END, "g^x mod p:\n", "client")
+        self.log_text.insert(tk.END, str(gcmodp) + "\n", "client")
+        self.log_text.insert(tk.END, "In reality this is a random value, just like the random values sent in TLS 1.2\n", "note")
+        self.log_text.insert(tk.END, "For the sake of simplicity, we will use a g^random_value mod p value\n", "note")
+        self.log_text.insert(tk.END, "This is a private session key of the client\n", "note")
+        self.log_text.insert(tk.END, "This is not sent\n", "note")
+        self.log_text.insert(tk.END, "client_session_key = " + str(bin(c)) + "\n", "note")
+        session_id = 0
+        self.log_text.insert(tk.END, "Session ID:\n", "client")
+        self.log_text.insert(tk.END, str(bin(session_id)) + "\n", "client")
+        # the client sends a list of supported ciphersuites
+        self.log_text.insert(tk.END, "Supported ciphersuites:\n", "client")
+        self.log_text.insert(tk.END, "ciphersuites = [0x1301, 0x1302, 0x1303, 0x1304]\n", "client")
+        # the client sends a list of extension, we will not include them in this simulation
+        self.log_text.insert(tk.END, "Extensions:\n", "client")
+        self.log_text.insert(tk.END, "extensions = []\n", "client")
 
         self.log_text.insert(tk.END, "\nServerHello\n\n", "explanation")
-
         self.log_text.insert(tk.END, "The server sends a ServerHello message to the client, containing the following information:\n", "explanation")
 
-        time.sleep(1)  # Add a one-second delay here
+        self.log_text.insert(tk.END, "Highest version supported:\n", "server")
+        self.log_text.insert(tk.END, "1.3\n", "server")
+        s = random.getrandbits(10)
+        gs = pow(g, s)
+        gsmodp= gs % prime
+        self.log_text.insert(tk.END, "g^s mod p:\n", "server")
+        self.log_text.insert(tk.END, str(gsmodp) + "\n", "server")
+        self.log_text.insert(tk.END, "not sent: server_session_key = " + str(bin(s)) + "\n", "note")
+        self.log_text.insert(tk.END, "Session ID:\n", "server")
+        session_id = random.getrandbits(32)
+        self.log_text.insert(tk.END, str(bin(session_id)) + "\n", "server")
+        self.log_text.insert(tk.END, "Chosen ciphersuite:\n", "server")
+        self.log_text.insert(tk.END, "0x1301\n", "server")
+        self.log_text.insert(tk.END, "Extensions:\n", "server")
+        self.log_text.insert(tk.END, "extensions = []\n", "server")
+
+        self.log_text.insert(tk.END, "\n\nClient sends hash of agreed upon keys\n\n", "explanation")
+        self.log_text.insert(tk.END, "With g^s mod p and c, the client can calculate the shared secret\n", "note")
+        self.log_text.insert(tk.END, "In order to do this it calculates (g^s mod p)^c mod p\n", "note")
+        shared_keyClient = pow(gsmodp, c) % prime
+        self.log_text.insert(tk.END, "Shared key (not sent):\n", "note")
+        self.log_text.insert(tk.END, str(bin(shared_keyClient)) + "\n", "note")
+        self.log_text.insert(tk.END, "The client sends a hash of the shared key to the server\n", "explanation")
+        # we will use SHA-256 as the hash function to hash the shared key
+        hashClient = hashlib.sha256(str(shared_keyClient).encode()).hexdigest()
+        self.log_text.insert(tk.END, "Hash of shared key:\n", "client")
+        self.log_text.insert(tk.END, hashClient + "\n", "client")
+
+        self.log_text.insert(tk.END, "\n\n Handshake complete!\n\n", "explanation")
+
+        self.log_text.insert(tk.END, "The handshake is complete, the client and server can now communicate securely\n", "explanation")
+        self.log_text.insert(tk.END, "The server should check that its shared key matches the client's shared key\n", "explanation")
+        shared_keyServer = pow(gcmodp, s) % prime
+        hashServer = hashlib.sha256(str(shared_keyServer).encode()).hexdigest()
+        self.log_text.insert(tk.END, "Hash of shared key according to server:\n", "note")
+        self.log_text.insert(tk.END, hashServer + "\n", "server")
+        self.log_text.insert(tk.END, "Hash of shared key according to client:\n", "note")
+        self.log_text.insert(tk.END, hashClient + "\n", "client")
+
+
+
+
+
+
 
         self.log_text.see(tk.END)
 
